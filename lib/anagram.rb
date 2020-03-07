43f729dcc1b96ffa003ccbab208668ba4937435b
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-07 21:10:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initalize(word)
+    self.word = word
+  end
+
+  def match(words)
+    words.detect do |word|
+      self.word.split('').sort == word.split('').sort
+    end
+  end
+
+end
